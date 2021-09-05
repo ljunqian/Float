8f1dcc826763e942e71cfffb586dd4dc034ad5b2
ljunqian <ljunqian@gmail.com>
2021-09-05 13:25:02 +0800

    Shift FloatFE to root directory
    It makes it easier to add backend packages to the project.

--- App.js
+++ App.js
@@ -5,7 +5,6 @@
  * @format
  * @flow strict-local
  */
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
