a8f839bf93ced3b5318d8b7b0621dc9f93cc140d
ljunqian <ljunqian@gmail.com>
2021-09-06 18:27:03 +0800

    commit before pull request/merge

--- App.js
+++ App.js
@@ -1,32 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from 'react'
+import { View, StyleSheet, TextInput, Pressable, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
-import SplashScreen from './src/screens/SplashScreen';
-import TodayScreen from './src/screens/TodayScreen';
-import ProfileScreen from './src/screens/ProfileScreen/ProfileScreen';
-import LoginScreen from './src/screens/LoginScreen';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
+Amplify.configure(config);
 
-const Stack = createNativeStackNavigator();
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+
+//import { Auth } from 'aws-amplify';
+
+import SplashScreen from './screens/SplashScreen';
+import TodayScreen from './screens/TodayScreen';
+import ProfileScreen from './screens/ProfileScreen/ProfileScreen';
+import LoginScreen from './screens/LoginScreen';
+
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Today" component={TodayScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+  const [name, setName] = useState('');
+  const [sub, setSub] = useState('');
+  const [bio, setBio] = useState('');
+  const [gender, setGender] = useState();
 
-export default App;
+  const save = () => {
+    console.warn('not valid')
+  }
+  //if (!isValid()) {
+  //console.warn('not valid')
+  //return;
+  //}
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name......"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Bio......"
+        multiline
+        numberOfLines={5}
+        value={bio}
+        onChangeText={setBio}
+      />
+      <Picker
+        selectedValue={gender}
+        onValueChange={(itemValue) => setGender(itemValue)
+        }>
+        <Picker.Item label="Male" value="MALE" />
+        <Picker.Item label="Female" value="Female" />
+        <Picker.Item label="Other" value="OTHER" />
+      </Picker>
+      <AmplifySignOut buttonText="Custom Text"></AmplifySignOut>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: { width: '100%', flex: 1, padding: 18 },
+  container: { padding: 10 },
+  input: { margin: 10, borderWidth: 2 },
+  button: { padding: 10 }
+})
+
+export default withAuthenticator(App);
