107113c899db0f169577454d5dfcf7e70c5c84f0
NovemForxuz <mherizpy@live.com>
2021-09-21 23:59:24 +0800

    Added play icon

--- src/screens/Guides/FocusScreen/index.js
+++ src/screens/Guides/FocusScreen/index.js
@@ -9,6 +9,7 @@ import SleepAvatar from '../../../assets/images/sleep-avatar.png';
 import Sleep1 from '../../../assets/images/sleep1.png';
 import Sleep2 from '../../../assets/images/sleep2.png';
 import Sleep3 from '../../../assets/images/sleep3.png';
+import play from '../../../assets/icons/play.png';
 
 
 const GuideCardComponent = (props) => {
@@ -60,7 +61,8 @@ const FocusScreen = ({navigation}) => {
             <Text style={typo.H1}>
               Focus Session
             </Text>
-            <TouchableOpacity style={[layout.big_button, {backgroundColor: color.Focus3, marginBottom: 30, zIndex:2}]}>
+            <TouchableOpacity style={[layout.big_button, {backgroundColor: color.Focus3, marginBottom: 30, zIndex:2, flexDirection: 'row'}]}>
+                <Image source={play} style={{marginRight: 5}} />
                 <Text style={[typo.T4, {color: 'white', fontWeight: '400'}]}>
                   Play
                 </Text>
