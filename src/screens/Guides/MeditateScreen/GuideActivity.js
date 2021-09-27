c3cbc8eee9ac7ceff0e150c2794fde93be0a3ea1
nwifty <77317736+nwifty@users.noreply.github.com>
2021-09-28 00:23:19 +0800

    Update GuideActivity UI

--- src/screens/Guides/MeditateScreen/GuideActivity.js
+++ src/screens/Guides/MeditateScreen/GuideActivity.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import typo from '../../../styles/typography';
+import { style } from 'styled-system';
+import MedBG from '../../../assets/images/meditate-planet.png';
+import Bigplay from '../../../assets/icons/bigplay.png';
+import { Guides } from './constants';
 
 const Activity = ({navigate}) => {
     return  (
-        <View style={styles.container}>
-            <View style={styles.actComponent}>
-                <Text style={{color: 'white'}}>
-                    (this is the video)
+        
+       
+        <View style={styles.container}> 
+        <ImageBackground source={MedBG}  resizeMode="cover" style={{width : '100%', height: '100%'}} >
+            <View style={styles.actComponent, {justifyContent: 'center', alignItems: 'center'}}>
+            
+                <View>
+                <Text style={[typo.H4,{top: 150, color: 'white', fontWeight:'400'}]}>
+                    Session Name
                 </Text>
+                </View>
+            
+                <TouchableOpacity >
+                <Image source={Bigplay} style={{top: 240 }} />
+                <Text style={[typo.H2, {top: 280, color: 'white', fontWeight: '400', marginLeft: 18 }]}>
+                  13:42
+                </Text>
+                </TouchableOpacity>
             </View>
+             </ImageBackground>
         </View>
+      
+        
     )
 }
 
 const styles = StyleSheet.create({
+
     container : {
         display: 'flex',
         width: '100%',
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#272727'
+        backgroundColor: '#272727',
+
     },
     actComponent: {
         backgroundColor: '#272727'
